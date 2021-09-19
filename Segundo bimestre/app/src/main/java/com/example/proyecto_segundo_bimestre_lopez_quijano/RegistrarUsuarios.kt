b4ab09e2e4ec3e67fe5c3f45e71a663fc5fae562
Diana <diana.lopez03@epn.edu.ec>
2021-09-19 02:59:28 -0500

    Creación de usuario

---
+++ Segundo bimestre/app/src/main/java/com/example/proyecto_segundo_bimestre_lopez_quijano/RegistrarUsuarios.kt
@@ -0,0 +1,24 @@
+package com.example.proyecto_segundo_bimestre_lopez_quijano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+
+class RegistrarUsuarios : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registrar_usuarios)
+
+
+    }
+
+
+    }
+
+
+
