7802081135f9ecf40f93931ebb52805d574a1276
Diana <diana.lopez03@epn.edu.ec>
2021-09-19 00:32:56 -0500

    Pantalla crear actividad, visualizar actividad y crear lista

---
+++ Segundo bimestre/app/src/main/java/com/example/proyecto_segundo_bimestre_lopez_quijano/AgregarLista.kt
@@ -0,0 +1,26 @@
+package com.example.proyecto_segundo_bimestre_lopez_quijano
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class AgregarLista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_lista)
+
+        /*
+        val arrelo = arrayListOf<String>("a","b", "c","d","e")
+
+        val adaptador = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            arrelo
+        )
+
+        val list = findViewById<ListView>(R.id.lv_miembrosListaCrear)
+        list.adapter = adaptador
+        */
+    }
+}
--- Segundo bimestre/app/src/main/java/com/example/proyecto_segundo_bimestre_lopez_quijano/CrearActividad.kt
+++ Segundo bimestre/app/src/main/java/com/example/proyecto_segundo_bimestre_lopez_quijano/CrearActividad.kt
@@ -4,10 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import com.example.proyecto_segundo_bimestre_lopez_quijano.entities.Lista
+import com.example.proyecto_segundo_bimestre_lopez_quijano.entities.Usuario
 
 class CrearActividad : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_actividad)
+
+        // TODO: Obtener los datos del intent
+
+        val usuarioEjemplo = Usuario(
+            "ID_usuario_1",
+            "Juan",
+            "Suasnabas",
+            "user@email.com",
+            "01/01/2020",
+            "hash123"
+        )
+        val lista = Lista("ID_lista_1", "Lista 1", arrayListOf(usuarioEjemplo), "ID_usuario_1")
+
+        // Cambiar el título de la actividad al nombre de la lista
+        val nombreLista = lista.nombre
+        this.setTitle(nombreLista);
     }
 }
